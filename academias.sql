feca40aadfecadf2d0613660e706c03ad9b7cf1a
DAMIAN BRAVO <102478250+damianBRAVO@users.noreply.github.com>
2022-12-21 23:02:57 -0300

    Update academias.sql

--- academias.sql
+++ academias.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 22-12-2022 a las 00:22:40
+-- Tiempo de generación: 22-12-2022 a las 00:25:14
 -- Versión del servidor: 10.4.27-MariaDB
 -- Versión de PHP: 7.4.33
 
@@ -41,6 +41,74 @@ CREATE TABLE `academias` (
 INSERT INTO `academias` (`id`, `nombre`, `telefono`, `web`) VALUES
 (1, 'Academia online', '242353465475676879789096686868', 'academia_arg@live.com');
 
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumnos`
+--
+
+CREATE TABLE `alumnos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `apellido` varchar(80) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `telefono` int(45) NOT NULL,
+  `academia_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `alumno_x_curso`
+--
+
+CREATE TABLE `alumno_x_curso` (
+  `id` int(11) NOT NULL,
+  `alumno_id` int(11) NOT NULL,
+  `curso_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cursos`
+--
+
+CREATE TABLE `cursos` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `descripcion` text NOT NULL,
+  `profesor_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `notas`
+--
+
+CREATE TABLE `notas` (
+  `id` int(11) NOT NULL,
+  `nota` float NOT NULL,
+  `curso_id` int(11) NOT NULL,
+  `alumno_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `profesores`
+--
+
+CREATE TABLE `profesores` (
+  `id` int(80) NOT NULL,
+  `nombre` varchar(80) NOT NULL,
+  `apellido` varchar(80) NOT NULL,
+  `telefono` varchar(45) NOT NULL,
+  `email` varchar(225) NOT NULL,
+  `academia_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
 --
 -- Índices para tablas volcadas
 --
@@ -52,6 +120,43 @@ ALTER TABLE `academias`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `academia_alumno_fk` (`academia_id`);
+
+--
+-- Indices de la tabla `alumno_x_curso`
+--
+ALTER TABLE `alumno_x_curso`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `alumno_curso_fk` (`alumno_id`),
+  ADD KEY `curso_alumno_fk` (`curso_id`);
+
+--
+-- Indices de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `profesor_curso_fk` (`profesor_id`);
+
+--
+-- Indices de la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `nota_alumno_fk` (`alumno_id`),
+  ADD KEY `nota_curso_fk` (`curso_id`);
+
+--
+-- Indices de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `academia_fk` (`academia_id`);
+
+--
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
@@ -60,6 +165,72 @@ ALTER TABLE `academias`
 --
 ALTER TABLE `academias`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `alumno_x_curso`
+--
+ALTER TABLE `alumno_x_curso`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `notas`
+--
+ALTER TABLE `notas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  MODIFY `id` int(80) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `alumnos`
+--
+ALTER TABLE `alumnos`
+  ADD CONSTRAINT `academia_alumno_fk` FOREIGN KEY (`academia_id`) REFERENCES `academias` (`id`);
+
+--
+-- Filtros para la tabla `alumno_x_curso`
+--
+ALTER TABLE `alumno_x_curso`
+  ADD CONSTRAINT `alumno_curso_fk` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `curso_alumno_fk` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Filtros para la tabla `cursos`
+--
+ALTER TABLE `cursos`
+  ADD CONSTRAINT `profesor_curso_fk` FOREIGN KEY (`profesor_id`) REFERENCES `profesores` (`id`);
+
+--
+-- Filtros para la tabla `notas`
+--
+ALTER TABLE `notas`
+  ADD CONSTRAINT `nota_alumno_fk` FOREIGN KEY (`alumno_id`) REFERENCES `alumnos` (`id`),
+  ADD CONSTRAINT `nota_curso_fk` FOREIGN KEY (`curso_id`) REFERENCES `cursos` (`id`);
+
+--
+-- Filtros para la tabla `profesores`
+--
+ALTER TABLE `profesores`
+  ADD CONSTRAINT `academia_fk` FOREIGN KEY (`academia_id`) REFERENCES `academias` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
